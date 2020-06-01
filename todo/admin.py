426e61e4e5775db75cd2fb665e41dd116aa328a6
Sergi Orra <sergiorra17@gmail.com>
2020-06-01 16:01:31 +0200

    Todo model

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Todo
 
-# Register your models here.
+
+class TodoAdmin(admin.ModelAdmin):
+    # To show the readonly field 'created' in admin page
+    readonly_fields = ('created',)
+
+
+admin.site.register(Todo, TodoAdmin)
