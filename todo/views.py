75f6395c0a16288f87eaa3c850c49c17559a615e
Sergi Orra <sergiorra17@gmail.com>
2020-06-01 15:38:10 +0200

    authentication flow working

--- todo/views.py
+++ todo/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 
 
 def home(request):
@@ -32,19 +32,13 @@ def loginuser(request):
     if request.method == 'GET':
         return render(request, 'todo/loginuser.html', {'form': AuthenticationForm()})
     else:
-        # create a new user
-        if request.POST['password1'] == request.POST['password2']:
-            try:
-                user = User.objects.create_user(
-                    request.POST['username'], password=request.POST['password1'])
-                user.save()
-                login(request, user)
-                return redirect('currenttodos')
-            except IntegrityError:
-                return render(request, 'todo/signupuser.html', {'form': UserCreationForm, 'error': 'That username has already been taken, please choose a new username'})
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'todo/loginuser.html', {'form': AuthenticationForm(), 'error': 'Username and password did not match'})
         else:
-            return render(request, 'todo/signupuser.html', {'form': UserCreationForm, 'error': 'Passwords did not match'})
-            # password didn't match
+            login(request, user)
+            return redirect('currenttodos')
 
 
 def currenttodos(request):
