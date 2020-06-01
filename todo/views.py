73ad6d55be5214edfa7861fb1431a8f1fca12297
Sergi Orra <sergiorra17@gmail.com>
2020-06-01 15:25:17 +0200

    initial setup with user authentication

---
+++ todo/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login, logout
+
+
+def home(request):
+    return render(request, 'todo/home.html')
+
+
+def signupuser(request):
+    if request.method == 'GET':
+        return render(request, 'todo/signupuser.html', {'form': UserCreationForm})
+    else:
+        # create a new user
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(
+                    request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('currenttodos')
+            except IntegrityError:
+                return render(request, 'todo/signupuser.html', {'form': UserCreationForm, 'error': 'That username has already been taken, please choose a new username'})
+        else:
+            return render(request, 'todo/signupuser.html', {'form': UserCreationForm, 'error': 'Passwords did not match'})
+            # password didn't match
+
+
+def loginuser(request):
+    return render(request, 'todo/currenttodos.html')
+
+def currenttodos(request):
+    return render(request, 'todo/currenttodos.html')
+
+
+def logoutuser(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
